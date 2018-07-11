ad55b1c77e7d8b0b4e586d1e17c1379bf6c468fb
ZeroLeonic <leonicfox@outlook.com>
2018-07-11 14:57:37 +0100

    Added global.service.ts

---
+++ src/app/global.service.ts
@@ -0,0 +1,5 @@
+export class GlobalService {
+    public title = 'DystopiaProject';
+    public appVersion = "1.0.0.0a";
+    public gitRepo = "https://github.com/DystopiaProject/dystopiaproject.github.io/";
+}
